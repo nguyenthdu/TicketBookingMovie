408c8e4858c4844778b8de364c617ad5c0a193d4
Nguyen Thanh Du <nguyenthdu6242@gmail.com>
2024-05-17 02:31:26 +0700

    update vnp_ipAddr vnpay

--- src/main/java/com/app/TicketBookingMovie/services/impl/VNPAYServiceImpl.java
+++ src/main/java/com/app/TicketBookingMovie/services/impl/VNPAYServiceImpl.java
@@ -68,14 +68,14 @@ public class VNPAYServiceImpl implements VNPAYService {
         vnp_Params.put("vnp_ReturnUrl", VNPayConfig.vnp_ReturnUrl);
         vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
 
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+8"));
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
 
         cld.add(Calendar.MINUTE, 15);
         String vnp_ExpireDate = formatter.format(cld.getTime());
-        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate + 1);
 
         List fieldNames = new ArrayList(vnp_Params.keySet());
         Collections.sort(fieldNames);
