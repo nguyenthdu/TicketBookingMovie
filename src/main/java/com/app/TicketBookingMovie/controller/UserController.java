751195ecff0bf5abc0e0c5c24d50376771adb35c
Nguyen Thanh Du <nguyenthdu6242@gmail.com>
2024-05-23 20:19:48 +0700

    fix update password

--- src/main/java/com/app/TicketBookingMovie/controller/UserController.java
+++ src/main/java/com/app/TicketBookingMovie/controller/UserController.java
@@ -46,12 +46,11 @@ public class UserController {
         this.jwtUtils = jwtUtils;
         this.userService = userService;
 
-
     }
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestParam("email") String email,
-                                              @RequestParam("password") String password) {
+            @RequestParam("password") String password) {
         SigninRequest loginRequest = new SigninRequest();
         loginRequest.setEmail(email);
         loginRequest.setPassword(password);
@@ -81,17 +80,19 @@ public class UserController {
                     userDetails.isGender(),
                     userDetails.getCreatedDate()));
         } catch (BadCredentialsException e) {
-            return ResponseEntity.badRequest().body(new MessageResponse("Email hoặc mật khẩu không đúng!!!", HttpStatus.BAD_REQUEST.value(), Instant.now().toString()));
+            return ResponseEntity.badRequest().body(new MessageResponse("Email hoặc mật khẩu không đúng!!!",
+                    HttpStatus.BAD_REQUEST.value(), Instant.now().toString()));
         } catch (DisabledException e) {
-            return ResponseEntity.badRequest().body(new MessageResponse("Tài khoản này chưa được kích hoạt!"
-                    , HttpStatus.BAD_REQUEST.value(), Instant.now().toString()));
+            return ResponseEntity.badRequest().body(new MessageResponse("Tài khoản này chưa được kích hoạt!",
+                    HttpStatus.BAD_REQUEST.value(), Instant.now().toString()));
         }
 
     }
-    //đăng nhập chỉ user có quyển mor hoặc admin mới có thể đăng nhập
+
+    // đăng nhập chỉ user có quyển mor hoặc admin mới có thể đăng nhập
     @PostMapping("/signinWeb")
     public ResponseEntity<?> authenticateUserWeb(@Valid @RequestParam("email") String email,
-                                                 @RequestParam("password") String password) {
+            @RequestParam("password") String password) {
         SigninRequest loginRequest = new SigninRequest();
         loginRequest.setEmail(email);
         loginRequest.setPassword(password);
@@ -110,7 +111,9 @@ public class UserController {
                 return ResponseEntity.badRequest().build();
             }
             if (!roles.contains("ROLE_ADMIN") && !roles.contains("ROLE_MODERATOR")) {
-                return ResponseEntity.badRequest().body(new MessageResponse("Bạn không có quyền truy cập vào trang này!!!", HttpStatus.BAD_REQUEST.value(), Instant.now().toString()));
+                return ResponseEntity.badRequest()
+                        .body(new MessageResponse("Bạn không có quyền truy cập vào trang này!!!",
+                                HttpStatus.BAD_REQUEST.value(), Instant.now().toString()));
             }
             return ResponseEntity.ok(new JwtResponse(jwt,
                     userDetails.getId(),
@@ -124,15 +127,16 @@ public class UserController {
                     userDetails.isGender(),
                     userDetails.getCreatedDate()));
         } catch (BadCredentialsException e) {
-            return ResponseEntity.badRequest().body(new MessageResponse("Email hoặc mật khẩu không đúng!!!", HttpStatus.BAD_REQUEST.value(), Instant.now().toString()));
+            return ResponseEntity.badRequest().body(new MessageResponse("Email hoặc mật khẩu không đúng!!!",
+                    HttpStatus.BAD_REQUEST.value(), Instant.now().toString()));
         } catch (DisabledException e) {
-            return ResponseEntity.badRequest().body(new MessageResponse("Tài khoản này chưa được kích hoạt!"
-                    , HttpStatus.BAD_REQUEST.value(), Instant.now().toString()));
+            return ResponseEntity.badRequest().body(new MessageResponse("Tài khoản này chưa được kích hoạt!",
+                    HttpStatus.BAD_REQUEST.value(), Instant.now().toString()));
         }
 
     }
 
-    //TODO: get all users
+    // TODO: get all users
     @GetMapping
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<PageResponse<UserDto>> getAllUsers(
@@ -152,22 +156,20 @@ public class UserController {
         return ResponseEntity.ok(userPageResponse);
     }
 
-
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR') or hasRole('USER')")
     public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
-    //TODO: register morderater
+    // TODO: register morderater
     @PostMapping("/mor")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<MessageResponse> registerMor(@Valid
-                                                       @RequestParam("username") String username,
-                                                       @RequestParam("email") String email,
-                                                       @RequestParam("phone") String phone,
-                                                       @RequestParam("gender") boolean gener,
-                                                       @RequestParam("password") String password) {
+    public ResponseEntity<MessageResponse> registerMor(@Valid @RequestParam("username") String username,
+            @RequestParam("email") String email,
+            @RequestParam("phone") String phone,
+            @RequestParam("gender") boolean gener,
+            @RequestParam("password") String password) {
         SignupRequest signUpRequest = new SignupRequest();
         signUpRequest.setUsername(username);
         signUpRequest.setEmail(email);
@@ -176,14 +178,16 @@ public class UserController {
         signUpRequest.setPassword(password);
         try {
             userService.createMor(signUpRequest);
-            return ResponseEntity.ok(new MessageResponse("Tạo nhân viên thành công!", HttpStatus.OK.value(), Instant.now().toString()));
+            return ResponseEntity.ok(
+                    new MessageResponse("Tạo nhân viên thành công!", HttpStatus.OK.value(), Instant.now().toString()));
         } catch (AppException e) {
-            return ResponseEntity.status(e.getStatus()).body(new MessageResponse(e.getMessage(), e.getStatus(), Instant.now().toString()));
+            return ResponseEntity.status(e.getStatus())
+                    .body(new MessageResponse(e.getMessage(), e.getStatus(), Instant.now().toString()));
         }
     }
 
-    //TODO: register user
-//    @PreAuthorize("hasRole('MODERATOR')")
+    // TODO: register user
+    // @PreAuthorize("hasRole('MODERATOR')")
     @PostMapping()
     public ResponseEntity<MessageResponse> registerUser(
             @RequestParam("username") String username,
@@ -202,14 +206,16 @@ public class UserController {
 
         try {
             userService.createUser(signUpRequest);
-            return ResponseEntity.ok(new MessageResponse("Đăng ký thành công. Vui lòng vào gmail để xác thực tài khoản sau đó mới có thể đăng nhập.", HttpStatus.OK.value(), Instant.now().toString()));
+            return ResponseEntity.ok(new MessageResponse(
+                    "Đăng ký thành công. Vui lòng vào gmail để xác thực tài khoản sau đó mới có thể đăng nhập.",
+                    HttpStatus.OK.value(), Instant.now().toString()));
         } catch (AppException e) {
             return ResponseEntity.ok(new MessageResponse(e.getMessage(), e.getStatus(), e.getTimestamp()));
         }
 
     }
 
-    @RequestMapping(value = "/confirm-account", method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value = "/confirm-account", method = { RequestMethod.GET, RequestMethod.POST })
     public ResponseEntity<?> confirmUserAccount(@RequestParam("token") String confirmationToken) {
         return userService.confirmEmail(confirmationToken);
     }
@@ -217,7 +223,10 @@ public class UserController {
     // update
     @PutMapping()
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR') or hasRole('USER')")
-    public ResponseEntity<MessageResponse> updateUser(@RequestParam("id") Long id, @RequestParam("username") String username, @RequestParam("gender") boolean gender, @RequestParam("birthday") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate birthday, @RequestParam("phone") String phone) {
+    public ResponseEntity<MessageResponse> updateUser(@RequestParam("id") Long id,
+            @RequestParam("username") String username, @RequestParam("gender") boolean gender,
+            @RequestParam("birthday") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate birthday,
+            @RequestParam("phone") String phone) {
         UserDto userDTO = new UserDto();
         userDTO.setUsername(username);
         userDTO.setGender(gender);
@@ -225,9 +234,11 @@ public class UserController {
         userDTO.setPhone(phone);
         try {
             userService.updateUser(id, userDTO);
-            return ResponseEntity.ok(new MessageResponse("Cập nhật người dùng thành công với di: " + id, HttpStatus.OK.value(), Instant.now().toString()));
+            return ResponseEntity.ok(new MessageResponse("Cập nhật người dùng thành công với di: " + id,
+                    HttpStatus.OK.value(), Instant.now().toString()));
         } catch (AppException e) {
-            return ResponseEntity.status(e.getStatus()).body(new MessageResponse(e.getMessage(), e.getStatus(), Instant.now().toString()));
+            return ResponseEntity.status(e.getStatus())
+                    .body(new MessageResponse(e.getMessage(), e.getStatus(), Instant.now().toString()));
         }
     }
 
@@ -236,21 +247,26 @@ public class UserController {
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         try {
             userService.deleteUser(id);
-            return ResponseEntity.ok().body(new MessageResponse("Xóa người dùng thành công với id: " + id, HttpStatus.OK.value(), Instant.now().toString()));
+            return ResponseEntity.ok().body(new MessageResponse("Xóa người dùng thành công với id: " + id,
+                    HttpStatus.OK.value(), Instant.now().toString()));
         } catch (AppException e) {
-            return ResponseEntity.status(e.getStatus()).body(new MessageResponse(e.getMessage(), e.getStatus(), Instant.now().toString()));
+            return ResponseEntity.status(e.getStatus())
+                    .body(new MessageResponse(e.getMessage(), e.getStatus(), Instant.now().toString()));
         }
     }
 
     // update password
-    @PostMapping("/{id}/password")
+    @PostMapping("/password")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR') or hasRole('USER')")
-    public ResponseEntity<?> updateUserPassword(@PathVariable Long id, @RequestParam String oldPassword, @RequestParam String newPassword, @RequestParam String confirmPassword) {
+    public ResponseEntity<?> updateUserPassword(@PathVariable Long id, @RequestParam String oldPassword,
+            @RequestParam String newPassword, @RequestParam String confirmPassword) {
         try {
             userService.updatePassword(id, oldPassword, newPassword, confirmPassword);
-            return ResponseEntity.ok().body(new MessageResponse("Đổi mật khẩu thành công!", HttpStatus.OK.value(), Instant.now().toString()));
+            return ResponseEntity.ok().body(
+                    new MessageResponse("Đổi mật khẩu thành công!", HttpStatus.OK.value(), Instant.now().toString()));
         } catch (AppException e) {
-            return ResponseEntity.status(e.getStatus()).body(new MessageResponse(e.getMessage(), e.getStatus(), Instant.now().toString()));
+            return ResponseEntity.status(e.getStatus())
+                    .body(new MessageResponse(e.getMessage(), e.getStatus(), Instant.now().toString()));
         }
     }
 
@@ -264,20 +280,22 @@ public class UserController {
         userService.createAdmin();
     }
 
-//    @PostMapping("/userInTicket")
-//    public ResponseEntity<MessageResponse> createUserInTicket(
-//            @RequestParam("username") String username,
-//            @RequestParam("email") String email) {
-//        UserDto userDto = new UserDto();
-//        userDto.setUsername(username);
-//        userDto.setEmail(email);
-//        try {
-//            userService.createUserInTicket(userDto);
-//            return ResponseEntity.ok(new MessageResponse("Đăng ký thành công!", HttpStatus.OK.value(), Instant.now().toString()));
-//        } catch (AppException e) {
-//            return ResponseEntity.status(e.getStatus()).body(new MessageResponse(e.getMessage(), e.getStatus(), Instant.now().toString()));
-//        }
-//    }
+    // @PostMapping("/userInTicket")
+    // public ResponseEntity<MessageResponse> createUserInTicket(
+    // @RequestParam("username") String username,
+    // @RequestParam("email") String email) {
+    // UserDto userDto = new UserDto();
+    // userDto.setUsername(username);
+    // userDto.setEmail(email);
+    // try {
+    // userService.createUserInTicket(userDto);
+    // return ResponseEntity.ok(new MessageResponse("Đăng ký thành công!",
+    // HttpStatus.OK.value(), Instant.now().toString()));
+    // } catch (AppException e) {
+    // return ResponseEntity.status(e.getStatus()).body(new
+    // MessageResponse(e.getMessage(), e.getStatus(), Instant.now().toString()));
+    // }
+    // }
 
     @PostMapping("/guest")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
